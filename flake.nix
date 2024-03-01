5e4547b36da1afa06e5c1e40a65d3850e6bf16e2
zackartz <zackmyers@lavabit.com>
2024-03-01 14:15:41 -0500

    update flake

--- flake.nix
+++ flake.nix
@@ -5,29 +5,29 @@
     rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.follows = "rust-overlay/flake-utils";
     nixpkgs.follows = "rust-overlay/nixpkgs";
-    naersk.url = "github:nix-community/naersk";
   };
 
- outputs = inputs: with inputs;
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        code = pkgs.callPackage ./. { inherit nixpkgs system naersk rust-overlay; };
-      in rec {
-        packages = {
-kb = pkgs.rustPlatform.buildRustPackage {
-  pname = "kb";
-  version = "0.0.1";
-  src = ./.;
+  outputs = inputs:
+    with inputs;
+      flake-utils.lib.eachDefaultSystem (
+        system: let
+          pkgs = nixpkgs.legacyPackages.${system};
+          code = pkgs.callPackage ./. {inherit nixpkgs system naersk rust-overlay;};
+        in rec {
+          packages = {
+            kb = pkgs.rustPlatform.buildRustPackage {
+              pname = "kb";
+              version = "0.0.1";
+              src = ./.;
 
-  cargoLock = {
-    lockFile = ./Cargo.lock;
-  };
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
 
-  nativeBuildInputs = [ pkgs.pkg-config ];
-  buildInputs = [ pkgs.systemd ];
-};
-        };
-      }
-    );
+              nativeBuildInputs = [pkgs.pkg-config];
+              buildInputs = [pkgs.systemd];
+            };
+          };
+        }
+      );
 }
