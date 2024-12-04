499dab14fc2935e802e259501e86f5ffe8fb2f2e
zack <hi@zoeys.computer>
2024-12-03 22:06:04 -0500

    update build

--- flake.nix
+++ flake.nix
@@ -1,33 +1,102 @@
 {
-  description = "zack kb gui thing";
-
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+
     rust-overlay.url = "github:oxalica/rust-overlay";
-    flake-utils.follows = "rust-overlay/flake-utils";
-    nixpkgs.follows = "rust-overlay/nixpkgs";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = inputs:
-    with inputs;
-      flake-utils.lib.eachDefaultSystem (
-        system: let
-          pkgs = nixpkgs.legacyPackages.${system};
-          code = pkgs.callPackage ./. {inherit nixpkgs system naersk rust-overlay;};
-        in rec {
-          packages = {
-            kb = pkgs.rustPlatform.buildRustPackage {
-              pname = "kb";
-              version = "0.0.1";
-              src = ./.;
+  # Add settings for your binary cache.
+  nixConfig = {
+  };
 
-              cargoLock = {
-                lockFile = ./Cargo.lock;
-              };
+  outputs = inputs @ {
+    nixpkgs,
+    flake-parts,
+    ...
+  }: let
+    # For details on these options, See
+    # https://github.com/oxalica/rust-overlay?tab=readme-ov-file#cheat-sheet-common-usage-of-rust-bin
+    #
+    # Channel of the Rust toolchain (stable or beta).
+    rustChannel = "stable";
+    # Version (latest or specific date/semantic version)
+    rustVersion = "latest";
+    # Profile (default or minimal)
+    rustProfile = "default";
+  in
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = import inputs.systems;
 
-              nativeBuildInputs = [pkgs.pkg-config];
-              buildInputs = [pkgs.systemd];
-            };
+      imports = [
+        inputs.treefmt-nix.flakeModule
+      ];
+
+      perSystem = {
+        config,
+        system,
+        pkgs,
+        lib,
+        craneLib,
+        commonArgs,
+        ...
+      }: {
+        _module.args = {
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [inputs.rust-overlay.overlays.default];
           };
-        }
-      );
+          craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
+            pkgs: pkgs.rust-bin.${rustChannel}.${rustVersion}.${rustProfile}
+          );
+          commonArgs = {
+            # Depending on your code base, you may have to customize the
+            # source filtering to include non-standard files during the build.
+            # See
+            # https://crane.dev/source-filtering.html?highlight=source#source-filtering
+            src = craneLib.path ./.;
+
+            nativeBuildInputs = with pkgs; [
+              pkg-config
+            ];
+
+            buildInputs = with pkgs; [
+              systemd
+            ];
+          };
+        };
+
+        # Build the executable package.
+        packages.default = craneLib.buildPackage (
+          commonArgs
+          // {
+            cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+          }
+        );
+
+        devShells.default = craneLib.devShell {
+          packages =
+            (commonArgs.nativeBuildInputs or [])
+            ++ (commonArgs.buildInputs or [])
+            ++ [pkgs.rust-analyzer-unwrapped];
+
+          RUST_SRC_PATH = "${
+            pkgs.rust-bin.${rustChannel}.${rustVersion}.rust-src
+          }/lib/rustlib/src/rust/library";
+        };
+
+        treefmt = {
+          projectRootFile = "Cargo.toml";
+          programs = {
+            actionlint.enable = true;
+            nixfmt.enable = true;
+            rustfmt.enable = true;
+          };
+        };
+      };
+    };
 }
